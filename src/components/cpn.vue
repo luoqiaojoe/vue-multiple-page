3a5e02965bddec347203f3a16b3934a0ebeb6f3a
bcluo <luoqiaojoe@163.com>
2019-02-25 17:52:24 +0800

    feat:first commit

---
+++ src/components/cpn.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'cpn'
+  data(){
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/test/test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+       <cpn></cpn>
+    </div>
+</template>
+
+<script>
+import cpn from 'components/cpn.vue'
+
+export default {
+  name: 'app',
+  components: { cpn }
+}
+</script>
+
+<style>
+#app {
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
